f43510da622d775901194492f267d50fdfe45c9b
Filip <filip.ratkovic.fr@gmail.com>
2023-07-07 18:39:21 +0200

    Nav bar

--- quote-app/src/Pages/Nav/Nav.jsx
+++ quote-app/src/Pages/Nav/Nav.jsx
@@ -4,20 +4,49 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { themeSlice } from '../../Store/themeSlice'
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 function Nav() {
   const themeState = useSelector((state) => state.theme)
   const dispatch = useDispatch()
 
   return (
     <div>
-        <div>favorites</div>
-        <div>login</div>
-        <Switch
+<Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Home
+          </Typography>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Favorites
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Sign up</Button>
+          <Switch
           onChange={() => {
             dispatch(themeSlice.actions.toggleTheme());
           }}
           checked={themeState.theme === "dark"}
         />
+        </Toolbar>
+      </AppBar>
+    </Box>
     </div>
   )
 }
