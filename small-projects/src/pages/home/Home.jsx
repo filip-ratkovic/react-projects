a339205a4ca48cbc6352d0231efe2c99af42e365
Filip Ratkovic <filip28r@gmail.com>
2024-08-15 13:27:45 +0200

    Buttons

--- small-projects/src/pages/home/Home.jsx
+++ small-projects/src/pages/home/Home.jsx
@@ -2,21 +2,42 @@ import React from "react";
 import react from "../../photos/react.png";
 import css from "../../photos/css.png";
 import javascript from "../../photos/javascript.png";
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 import "./home.css";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate()
   const cardsName = ["Widgets", "Carusels", "Dropdowns"];
   return (
     <main className="home-main-cont">
       <h1 id="home-header">Small Projects</h1>
-      <section className="projects-card-container">
+     <div style={{display:"flex"}}>
+     <section className="projects-card-container">
         <div className="card-container">
           <h1>Carousels</h1>
           <img src={react} alt="react logo" />
           <button>React</button>
+          <button id="btn"
+          onClick={()=> {navigate("/carusels")}}>
+            <OpenInNewIcon/>
+          </button>
         </div>
       </section>
+
+      <section className="projects-card-container">
+        <div className="card-container">
+          <h1>Dropdowns</h1>
+          <img src={javascript} alt="Javas logo" />
+          <button>JS</button>
+          <button id="btn"
+          onClick={()=> {navigate("/carusels")}}>
+            <OpenInNewIcon/>
+          </button>
+        </div>
+      </section>
+     </div>
     </main>
   );
 };
