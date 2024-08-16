d9e7defd9699b55e57d1496851009e21524a3b56
Filip Ratkovic <filip28r@gmail.com>
2024-08-16 08:48:52 +0200

    Data

---
+++ small-projects/src/data/homeData.js
@@ -0,0 +1,26 @@
+import react from "../photos/react.png"
+import javascript from "../photos/javascript.png"
+import css from "../photos/css.png"
+
+const homeData = [
+    {
+        name: "Carusels",
+        image: react,
+        language: "React",
+        link: "/carusels"
+    },
+    {
+        name: "Dropdowns",
+        image: javascript,
+        language: "JavaScript",
+        link: "/dropdowns"
+    },
+    {
+        name: "Dropdowns",
+        image: css,
+        language: "CSS",
+        link: "/dropdowns"
+    }
+]
+
+export default homeData
--- small-projects/src/pages/home/Home.jsx
+++ small-projects/src/pages/home/Home.jsx
@@ -3,40 +3,38 @@ import react from "../../photos/react.png";
 import css from "../../photos/css.png";
 import javascript from "../../photos/javascript.png";
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import homeData from "../../data/homeData";
+
 
 import "./home.css";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate()
-  const cardsName = ["Widgets", "Carusels", "Dropdowns"];
+
+
   return (
     <main className="home-main-cont">
       <h1 id="home-header">Small Projects</h1>
      <div style={{display:"flex"}}>
-     <section className="projects-card-container">
-        <div className="card-container">
-          <h1>Carousels</h1>
-          <img src={react} alt="react logo" />
-          <button>React</button>
-          <button id="btn"
-          onClick={()=> {navigate("/carusels")}}>
-            <OpenInNewIcon/>
-          </button>
-        </div>
-      </section>
 
-      <section className="projects-card-container">
+        {homeData.map((data, index) => {
+          return (
+            <section className="projects-card-container" key={index}>
         <div className="card-container">
-          <h1>Dropdowns</h1>
-          <img src={javascript} alt="Javas logo" />
-          <button>JS</button>
-          <button id="btn"
-          onClick={()=> {navigate("/carusels")}}>
+          <h1>{data.name}</h1>
+          <div className="home-image-cont">
+          <h2>{data.language}</h2>
+          <img src={data.image} alt="react logo" />
+          </div>
+          <button 
+          onClick={()=> {navigate(data.link)}}>
             <OpenInNewIcon/>
           </button>
         </div>
       </section>
+          )
+        })}
      </div>
     </main>
   );
