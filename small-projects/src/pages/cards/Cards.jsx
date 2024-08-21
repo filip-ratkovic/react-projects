d2564ab168a454d6de7fff1083eda56019eb08f7
Filip Ratkovic <filip28r@gmail.com>
2024-08-21 11:13:17 +0200

    Link

--- small-projects/src/pages/cards/Cards.jsx
+++ small-projects/src/pages/cards/Cards.jsx
@@ -5,11 +5,18 @@ import puppies from "../../images/puppies.png"
 const Cards = () => {
   
   return (
-    <div className='cards-cont'>
+    <div className='cards-main'>
+      <h1>CARDS</h1>
+      <div className='cards-cont'>
       <a href="https://filip-ratkovic.github.io/grupa-6/forms/dog.html" target='blank'>
        <img src={twitter} alt="twitter logo" />
       </a>
+      <a href="https://filip-ratkovic.github.io/grupa-6/forms/dog.html" target='blank'>
+
        <img src={puppies} alt="twitter logo" />
+      </a>
+       
+      </div>
        </div>
   )
 }
